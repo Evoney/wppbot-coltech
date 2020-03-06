17187e70633ccf2d3f0bbbb2e0e03769feb07156
evoney <evoney.mendonca@icomp.ufam.edu.br>
2020-03-06 13:03:16 -0400

    chatbot da coltech

---
+++ cliente.py
@@ -0,0 +1,8 @@
+class cliente:
+    def __init__(self, id, hora, minuto, dia, mes):
+        self.id = id
+        self.hora_inicio_atendimento = hora
+        self.minuto_inicio_atendimento = minuto
+        self.dia_inicio_atendimento = dia
+        self.mes_inicio_atendimento = mes
+        self.estado = 1
---
+++ setup.py
@@ -0,0 +1,20 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os", "pathlib", "urllib3", "queue"],
+                     'include_files': ["wppbot.py", "cliente.py", "menu.png", "quemSomos.png", "Carta de Serviços.pdf", "chromedriver1.exe"],
+                     "includes": [],
+                     "excludes": [],
+                    }
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+#base = None
+#if sys.platform == "win32":
+base = "Win32GUI"
+
+setup(  name = "Chatbot Coltech",
+        version = "1.0",
+        description = "Chatbot para WhatsApp da Coltech",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("main.py", base=base)])
